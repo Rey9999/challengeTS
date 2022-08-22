d5358f60be5dab331c2c9b8d54c81ae32ae61682
Rey9999 <103054282+Rey9999@users.noreply.github.com>
2022-08-22 13:50:03 +0200

    Update challenge.ts

--- challenge.ts
+++ challenge.ts
@@ -1,16 +1,18 @@
 // challenge.ts
-interface user {
+interface User {
     name: string;
-    age: number;
-}
-
-const prettyPrintWilder = (users:any) => {
-    users.map((user: any) => {
+    age?: number;
+    birthday?: string;
+  }
+  
+const prettyPrintWilder= (users: User[]) => {
+    users.map((user) => {
       console.log(`${user.name} is ${user.age} years old`);
     });
   };
-  
-  const wilders : any = [];
+
+ 
+  const wilders: User[] = [];
   const user1 = { name: "Pierre", age: 23 };
   const user2 = { name: "Paul", birthday: "10/02/1990" };
   const user3 = { name: "Jacques", age: 25 };
@@ -18,3 +20,5 @@ const prettyPrintWilder = (users:any) => {
   wilders.push(user2);
   wilders.push(user3);
   prettyPrintWilder(wilders);
+
+ 
